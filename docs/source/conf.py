611f23a28f8ab8f1067135ba8ec21d46af9fc6fa
cutright <dan.cutright@gmail.com>
2020-09-06 19:50:56 -0500

    sphinx docs dev, read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
 
-import dvhastats
+import sphinx_rtd_theme
 from dvhastats import _version
 
 
@@ -36,7 +36,7 @@ release = _version.__release__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['rinoh.frontend.sphinx', 'sphinx.ext.autodoc'
+extensions = ['rinoh.frontend.sphinx', 'sphinx.ext.autodoc', 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,7 +53,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
